4f85b1f0ff3d5a41f900563db374fda224b99cc7
Trent Walkup <trentwalkup@Trents-MacBook-Pro.local>
2020-08-31 13:04:24 -0500

    wip

--- test/specs/landing.title.js
+++ test/specs/landing.title.js
@@ -3,7 +3,7 @@ const utils = require("../utils.js");
 describe("My landing page", () => {
   it(`should have the correct title (${utils.viewPort})`, () => {
     browser.url("/");
-    let a = $('#body > div.header > span.header-title')
+    let a = $('span.header-title')
     console.log(a.getText())
     // expect(browser).toHaveTitle("Flogistix-Test");
   });
