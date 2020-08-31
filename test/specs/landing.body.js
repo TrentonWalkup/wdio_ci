fdd1836f114d5b72d8a6d9dbfab28660f93abb43
Trent Walkup <trentwalkup@Trents-MacBook-Pro.local>
2020-08-31 13:29:04 -0500

    wip

--- test/specs/landing.body.js
+++ test/specs/landing.body.js
@@ -2,7 +2,7 @@ const LandingPage = require("../pageobjects/landing.page");
 const utils = require("../utils.js");
 const sectionTitles = [
   "About Flogistix",
-  "Our Values",
+  "Our1 Values",
   "Our Mission",
   "Contact us",
 ];
