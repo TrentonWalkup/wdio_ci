b890f993cc4b6ee50194dd7b63ab29fe2189d86a
Trent Walkup <trentwalkup@Trents-MacBook-Pro.local>
2020-08-31 15:24:58 -0500

    wip

--- test/specs/landing.contact.js
+++ test/specs/landing.contact.js
@@ -5,6 +5,5 @@ describe("The contact section", () => {
   it(`should render (${utils.viewPort})`, () => {
     browser.url("/");
     expect(LandingPage.Contact.contactForm).toExist;
-    expect(1+3).toEqual(1)
   });
 });
--- test/specs/landing.title.js
+++ test/specs/landing.title.js
@@ -4,6 +4,6 @@ describe("My landing page", () => {
   it(`should have the correct title (${utils.viewPort})`, () => {
     browser.url("/");
     let a = $('span.header-title')
-    expect(browser).toHaveTitle("Flogistix-Test");
+    expect(browser).toHaveTitle("localhost");
   });
 });
