e431a644bc19512e8096b8a69ead8e1f90a4bb82
Trent Walkup <trentwalkup@Trents-MacBook-Pro.local>
2020-08-31 23:10:44 -0500

    wip

--- test/specs/landing.title.js
+++ test/specs/landing.title.js
@@ -2,8 +2,11 @@ const utils = require("../utils.js");
 
 describe("My landing page", () => {
   it(`should have the correct title (${utils.viewPort})`, () => {
-    browser.url("/");
-    let a = $('span.header-title')
-    expect(browser).toHaveTitle("localhost");
+    // browser.url("/");
+    // let a = $('span.header-title')
+    // expect(browser).toHaveTitle("localhost");
+    browser.url('https://www.wikipedia.org')
+    let a = $('#www-wikipedia-org > div.central-textlogo > h1 > span')
+    console.log(a.getText())
   });
 });
