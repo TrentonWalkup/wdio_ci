ed736ffffa087439be088ac29d6d49bd396d3c83
Trent Walkup <trentwalkup@Trents-MacBook-Pro.local>
2020-11-24 06:37:09 -0600

    3

--- test/specs/landing.contact.js
+++ test/specs/landing.contact.js
@@ -1,11 +1,11 @@
-const LandingPage = require("../pageobjects/landing.page");
-const utils = require("../utils.js");
+const LandingPage = require('../pageobjects/landing.page');
+const utils = require('../utils.js');
 
-describe("The contact section", () => {
+describe('The contact section', () => {
   it(`should render (${utils.viewPort})`, () => {
-    // browser.url("/");
-    let a = browser.getUrl()
-    console.log(a)
+    browser.url('/');
+    let a = browser.getUrl();
+    console.log(a);
     expect(LandingPage.Contact.contactForm).toExist;
   });
 });
--- test/specs/landing.title.js
+++ test/specs/landing.title.js
@@ -1,13 +1,11 @@
-const utils = require("../utils.js");
+const utils = require('../utils.js');
 
-describe("My landing page", () => {
+describe('My landing page', () => {
   it(`should have the correct title (${utils.viewPort})`, () => {
     // browser.url("/");
     // let a = $('span.header-title')
     // expect(browser).toHaveTitle("localhost");
-    browser.url('https://www.wikipedia.org')
-    let a = $('#www-wikipedia-org > div.central-textlogo > h1 > span')
-    console.log(a.getText())
-    console.log(process.env)
+    browser.url('https://www.wikipedia.org');
+    let a = $('#www-wikipedia-org > div.central-textlogo > h1 > span');
   });
 });
